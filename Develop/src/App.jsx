2f2b831f6f3e3cc064d021061f77855950daa202
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-10 21:54:53 -0500

    updated code in app.jsx, downloaded react router dom

---
+++ Develop/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import Navbar from './components/navbar.jsx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <FontAwesomeIcon/>
+       </div>
+  )}
+
+export default App
---
+++ Develop/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from 'fontawesome.macro'
+
+
+
+
+
+
+function Navbar() {
+  
+    return (
+        <nav>
+            <div>
+            <FontAwesome icon={fas`square`} />
+            </div>
+        </nav>
+    )
+  
+  }
+  
+  export default Navbar
