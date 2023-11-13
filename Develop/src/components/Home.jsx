5558bfc6d2d3c2ca7bd997389e2e943bf4c800c2
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-13 00:07:25 -0500

    added animations to homepage

--- Develop/src/components/Home.jsx
+++ Develop/src/components/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'animate.css';
+import Shape from '../assets/roundShapes.png';
+
 
 const Home = () => {
     const navigate = useNavigate()
@@ -8,9 +11,10 @@ const Home = () => {
         <div className="home-container">
             <div className="home">
                 <div className="text-container">
-                    <h1>HELLO,</h1>
-                    <h1>MY NAME IS</h1>
-                    <h1>VICTOR</h1>
+                    <img src={Shape} alt="shapes" className="round-shapes" />
+                    <h1 className='animate-hello'>HELLO,</h1>
+                    <h1 className='animate-my-name'>MY NAME IS</h1>
+                    <h1 className='animate-vic'>VICTOR</h1>
                 </div>
                     <button className="projects-btn" onClick={() => { navigate('/projects')}}>PROJECTS</button>
             </div>
