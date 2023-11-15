34b1dceeec96f33fb2963fcb067f9f2a07a059f3
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-15 12:32:29 -0500

    finished card links in projects.jsx

--- Develop/src/components/pages/Projects.jsx
+++ Develop/src/components/pages/Projects.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import quizImage from '../../assets/classroom-unsplash.jpg';
 import dishImage from '../../assets/Dish-Dash-image-maarten-van-den-heuve.jpg';
-// import calorieImage from '../../assets/calorie-quest-image.jpg';
+import calorieImage from '../../assets/dietpic.png';
+import passwordImage from '../../assets/computerCoding.jpg'
+import weatherImage from '../../assets/sunnyweather.png'
+import pixelImage from '../../assets/pixelMarketImage.png'
+
 
 const Projects = () => {
  
   const projectsData = [
     { id: 1, title: 'Dish Dash', image: dishImage, dishLink: 'https://brennysouza.github.io/dish-dash/' },
-    { id: 2, title: 'Calorie Quest', calorieQuestLink: 'https://calorie-quest-87555c29dfe4.herokuapp.com/' },
-    { id: 3, title: 'Weather Wizard', weatherWizardLink: 'https://pc-vic.github.io/Weather-Wizard/' },
-    { id: 4, title: 'Pixel Market', pixelMarketLink: 'https://drive.google.com/file/d/1T938A97pL9xfBJQtZ0wezvkWoU9in8bF/view' },
+    { id: 2, title: 'Calorie Quest', image: calorieImage, calorieQuestLink: 'https://calorie-quest-87555c29dfe4.herokuapp.com/' },
+    { id: 3, title: 'Weather Wizard', image: weatherImage, weatherWizardLink: 'https://pc-vic.github.io/Weather-Wizard/' },
+    { id: 4, title: 'Pixel Market', image: pixelImage, pixelMarketLink: 'https://drive.google.com/file/d/1T938A97pL9xfBJQtZ0wezvkWoU9in8bF/view' },
     { id: 5, title: 'JavaScript Quiz', image: quizImage, quizLink: 'https://pc-vic.github.io/JavaScript-Quiz-app/' },
-    { id: 6, title: 'Password Gen', passwordGenLink: 'https://pc-vic.github.io/Password-Gen/' },
+    { id: 6, title: 'Password Gen', image: passwordImage, passwordGenLink: 'https://pc-vic.github.io/Password-Gen/' },
 
   ];
 
