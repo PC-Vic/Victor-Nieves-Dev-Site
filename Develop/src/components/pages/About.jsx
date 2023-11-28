4f072ade6568cea593446600a319d031dfe9c6e8
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-27 20:07:25 -0500

    about me section media queries

--- Develop/src/components/pages/About.jsx
+++ Develop/src/components/pages/About.jsx
@@ -8,7 +8,7 @@ function About() {
           <h2 className="about-me-heading">About Me</h2>
             <div className='about-container'>
               <p>
-              I am a full stack developer who is passionate about creating innovative and user-friendly web applications. Although I'm relatively new to the field, I'm dedicated to constantly learning and honing my skills. I believe in the power of technology to make a positive impact, and I'm excited about the possibilities that full stack development offers. Through my portfolio, I aim to showcase my progress, demonstrate my enthusiasm for coding, and contribute to projects that make a difference. Join me on this journey as I evolve and grow as a developer!
+              I am a full stack developer who is passionate about creating innovative and user-friendly web applications. Through my portfolio, I aim to showcase my progress, demonstrate my enthusiasm for coding, and contribute to projects that make a difference. If you want to collaborate on a project or just want to say hi, feel free to reach out to me on LinkedIn or via email.
               </p>
             </div>
           </div>
