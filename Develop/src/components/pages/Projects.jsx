a34fd541a79a56f623b871ea44bedcc7b73e4903
Victor Nieves <c_cc_vic@yahoo.com>
2023-11-29 11:13:17 -0500

    alignment changes to about component and project componenet

--- Develop/src/components/pages/Projects.jsx
+++ Develop/src/components/pages/Projects.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import quizImage from '../../assets/classroom-unsplash.jpg';
 import dishImage from '../../assets/Dish-Dash-image-maarten-van-den-heuve.jpg';
 import calorieImage from '../../assets/dietpic.png';
-import passwordImage from '../../assets/computerCoding.jpg'
-import weatherImage from '../../assets/sunnyweather.png'
 import pixelImage from '../../assets/pixelMarketImage.png'
 
 
@@ -12,10 +10,8 @@ const Projects = () => {
   const projectsData = [
     { id: 1, title: 'Dish Dash', image: dishImage, dishLink: 'https://brennysouza.github.io/dish-dash/' },
     { id: 2, title: 'Calorie Quest', image: calorieImage, calorieQuestLink: 'https://calorie-quest-87555c29dfe4.herokuapp.com/' },
-    { id: 3, title: 'Weather Wizard', image: weatherImage, weatherWizardLink: 'https://pc-vic.github.io/Weather-Wizard/' },
-    { id: 4, title: 'Pixel Market', image: pixelImage, pixelMarketLink: 'https://drive.google.com/file/d/1T938A97pL9xfBJQtZ0wezvkWoU9in8bF/view' },
-    { id: 5, title: 'JavaScript Quiz', image: quizImage, quizLink: 'https://pc-vic.github.io/JavaScript-Quiz-app/' },
-    { id: 6, title: 'Password Gen', image: passwordImage, passwordGenLink: 'https://pc-vic.github.io/Password-Gen/' },
+    { id: 3, title: 'E-Commerce API', image: pixelImage, pixelMarketLink: 'https://drive.google.com/file/d/1T938A97pL9xfBJQtZ0wezvkWoU9in8bF/view' },
+    { id: 4, title: 'JavaScript Quiz', image: quizImage, quizLink: 'https://pc-vic.github.io/JavaScript-Quiz-app/' },
 
   ];
 
@@ -28,11 +24,9 @@ const Projects = () => {
             <a
             key={project.id}
             href={project.id === 1 ? project.dishLink :
-              project.id === 5 ? project.quizLink :
+              project.id === 4 ? project.quizLink :
               project.id === 2 ? project.calorieQuestLink :
-              project.id === 3 ? project.weatherWizardLink :
-              project.id === 4 ? project.pixelMarketLink :
-              project.id === 6 ? project.passwordGenLink : '#'}
+              project.id === 3 ? project.pixelMarketLink : '#'}
 
             target="_blank"
             rel="noopener noreferrer"
