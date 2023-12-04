b2a5ef70bca1ceeef6a516ef37d204b624827bbb
Victor Nieves <c_cc_vic@yahoo.com>
2023-12-04 13:33:42 -0500

    updated about me page

--- Develop/src/components/pages/About.jsx
+++ Develop/src/components/pages/About.jsx
@@ -1,18 +1,21 @@
-import Avatar from '../../assets/avatar.png';
+import Stack from '../../assets/techstack.png';
 
 function About() {
 
     return (
       <>
         <div>
-          <h2 className="about-me-heading">About Me</h2>
-            <div className='about-container'>
-              <p>
-              I am a full stack developer who is passionate about creating innovative and user-friendly web applications. Through my portfolio, I aim to showcase my progress, demonstrate my enthusiasm for coding, and contribute to projects that make a difference. If you want to collaborate on a project or just want to say hi, feel free to reach out to me on LinkedIn or via email.
-              </p>
-            </div>
-          </div>
-        </>
+            <h2 className="about-me-heading">About Me</h2>
+              <div className='about-container'>
+                <p>
+                I am a full stack developer who is passionate about creating innovative and user-friendly web applications. Through my portfolio, I aim to showcase my progress, demonstrate my enthusiasm for coding, and contribute to projects that make a difference. If you want to collaborate on a project or just want to say hi, feel free to reach out to me on LinkedIn or via email.
+                </p>
+              </div>
+              <figure>
+                <img src={Stack} alt="Tech Skills" className="tech-skills-image" />
+              </figure>
+        </div>
+      </>
     );
 
 }
